ffa96d8191f5f90fa5a0333e9cb0e8291c6f918b
Mustang00 <97599183+Holodeec@users.noreply.github.com>
2022-09-26 10:34:22 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,145 +1,138 @@
-import java.io.IOException;
 import java.util.Scanner;
 import java.util.TreeMap;
 
-
 public class Main {
-
     public static void main(String[] args) throws Exception {
-
         System.out.println("На ноль операции делать нельзя");
+        System.out.println("Введите выражение");
         Scanner cs = new Scanner(System.in);
-        System.out.println("Введите выражение.");
-        String inpu = cs.nextLine();
-        inpu = inpu.replace(" ", "");
-        System.out.println(calc(inpu));
+        String input =cs.nextLine();
 
+        input.replace(" ","");
+        System.out.println(calc(input));
     }
-    public static String calc(String input) throws Exception {
+    public static String calc(String line) throws Exception {
         Converter converter = new Converter();
-        String[] index = {"+", "-", "*", "/"};
-        String[] iP = {"\\+", "-", "\\*", "/"};
-        String[] roman = {"I", "II", "III", "V", "IV", "VI", "VII", "VIII", "IX", "X"};
+        String[] action = {"+","-","*","/"};
+        String[] ip = {"\\+","-","\\*","/"};
+        String[] rom ={"I","II","III","IV","V","VI","VII","VIII","IX","X"};
+        int index = -1;
 
-        int action = -1;
-        for (int i = 0; i < index.length; i++) {
-
-            if (input.contains(index[i])) {
-                action = i;
-                break;
-            }
+        for(int i = 0; i < action.length;i++) {
+        if(line.contains(action[i])) {
+        index = i;
+        }
+        }
+        if(index==-1){
+            throw new Exception("Введите один из знаков +, -, *, /");
         }
 
-        if (action == -1) {
-                throw new Exception("Введите один из знаков: +, -, *, /");
-        }
-
-        String mass[] = input.split(iP[action]);
-
-        boolean isRoman=false;
+        String[] input = line.split(ip[index]);
 
         int a = 0;
         int b = 0;
+        boolean roman = false;
         try {
-            a = Integer.parseInt(mass[0]);
-            b = Integer.parseInt(mass[1]);
-        }catch (Exception e){
-            isRoman = true;
+            a =Integer.parseInt(input[0]);
+            b =Integer.parseInt(input[1]);
+        }catch (Exception e) {
+            roman = true;
         }
 
-        if(isRoman == true) {
-            int count = 0;
-            for (int i = 0; i < roman.length; i++) {
-                if (mass[0].equals(roman[i])) {
-                    count++;
-                }
-                if (mass[1].equals(roman[i])) {
-                    count++;
-                }
+        if(roman) {
+            int count = 0 ;
+            for (int i = 0; i < rom.length; i++) {
+            if (input[0].equals(rom[i]))  {
+                count++;
             }
-            if(count != 2){
-                throw new Exception("Числа должны быть в одной системе счисления");
+            if (input[1].equals(rom[i]))  {
+                count++;
+            }
+            }
+            if(count != 2 ) {
+                throw new Exception("Числа должны быть в одной системе исчисления");
             }
 
-            a = converter.romanToInt(mass[0]);
-            b = converter.romanToInt(mass[1]);
-        }
 
-        if (a < 1 || a > 10 || b < 1 || b > 10) {
-            throw new Exception("Числа должны быть в диапазоне от 1 до 10");
-        }
+            a = converter.romanToInt(input[0]);
+            b = converter.romanToInt(input[1]);
 
-        int result=0;
-        switch (index[action]) {
-            case "+":
-                result = (a + b) ;
+        }
+        if(a < 1 || a > 10 || b < 1 || b > 10) {
+           throw new Exception("Введенные числа должны быть от 1 до 10");
+        }
+        int result = 0;
+        switch (action[index]){
+            case "+" :
+               result = a+b;
                 break;
-            case "-":
-                result = (a - b) ;
+            case "-" :
+               result = a-b;
                 break;
-            case "/":
-                result = (a / b);
+            case "*" :
+               result = a*b;
                 break;
-            case "*":
-                result = (a * b) ;
+            case "/" :
+               result = a/b;
                 break;
         }
-        if(isRoman==true) {
-            String romanDigit = converter.intToRoman(result);
-            return romanDigit;
-        }else {
+        if(roman == true) {
+             String text = converter.intToRoman(result);
+             return text;
+        } else {
             return result+"";
         }
+
     }
+
 }
 class Converter {
-    TreeMap<Character, Integer> romanKeyMap = new TreeMap<>();
-    TreeMap<Integer, String> arabianKeyMap = new TreeMap<>();
+    public int romanToInt(String x) {
+        TreeMap<Character, Integer> romanKey = new TreeMap<>();
 
-    public Converter() {
-        romanKeyMap.put('I', 1);
-        romanKeyMap.put('V', 5);
-        romanKeyMap.put('X', 10);
-        romanKeyMap.put('L', 50);
-        romanKeyMap.put('C', 100);
+        romanKey.put('I', 1);
+        romanKey.put('V', 5);
+        romanKey.put('X', 10);
+        romanKey.put('L', 50);
+        romanKey.put('C', 100);
 
-        arabianKeyMap.put(100, "C");
-        arabianKeyMap.put(90, "XC");
-        arabianKeyMap.put(50, "L");
-        arabianKeyMap.put(40, "XL");
-        arabianKeyMap.put(10, "X");
-        arabianKeyMap.put(9, "IX");
-        arabianKeyMap.put(5, "V");
-        arabianKeyMap.put(4, "IV");
-        arabianKeyMap.put(1, "I");
+        int leng = x.length()-1;
+        char[] arr = x.toCharArray();
+        int mass = 0;
+        int result = romanKey.get(arr[leng]);
+        for (int i = leng-1; i >= 0;i--) {
+            mass = romanKey.get(arr[i]);
+
+            if(mass<romanKey.get(arr[i+1])) {
+                result-=mass;
+            } else {
+                result+=mass;
+            }
+
+        }
+        return result;
     }
-    public String intToRoman(int number) {
-        String roman = "";
-        int arabianKey;
-        do {
-            arabianKey = arabianKeyMap.floorKey(number);
-            roman += arabianKeyMap.get(arabianKey);
-            number -= arabianKey;
+    public String intToRoman (int x) {
+        TreeMap<Integer, String> arabianKey = new TreeMap<>();
+        arabianKey.put(100, "C");
+        arabianKey.put(50, "L");
+        arabianKey.put(10, "X");
+        arabianKey.put(5, "V");
+        arabianKey.put(3, "III");
+        arabianKey.put(2,"II");
+        arabianKey.put(1, "I");
 
-        } while (number != 0);
+        String roman ="";
+        int arabian ;
+        do {
+            arabian = arabianKey.floorKey(x);
+
+            roman +=arabianKey.get(arabian);
+            x -=arabian;
+
+        }while(x != 0);
         return roman;
 
     }
-    public int romanToInt(String s) {
-        int end = s.length() - 1;
-        char[] arr = s.toCharArray();
-        int arabian;
-        int result = romanKeyMap.get(arr[end]);
-        for (int i = end - 1; i >= 0; i--) {
-            arabian = romanKeyMap.get(arr[i]);
 
-            if (arabian < romanKeyMap.get(arr[i + 1])) {
-                result -= arabian;
-            } else {
-                result += arabian;
-            }
-        }
-        return result;
-
-    }
 }
